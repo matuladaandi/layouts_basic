09acb00393d7064bc82f482b0f05ee263d502300
andi <echo.matuladaandi@gmail.com>
2023-08-18 10:10:32 +0800

    add folder app/singn_in

--- lib/main.dart
+++ lib/main.dart
@@ -2,4 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Time Tricer',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo
+      ),
+      home: Container(
+        color: Colors.white,
+      ),
+    );
+  }
 }
